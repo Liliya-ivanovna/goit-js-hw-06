48863a0b3496ee0d7d8af35f2b628f1f2f649f3e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-04 09:47:31 +0300

    Update task-01.js

--- src 2/js/task-01.js
+++ src 2/js/task-01.js
@@ -1,6 +1,3 @@
 const ulEl = document.querySelector('.categories')
 const numberOfCategories = ulEl.elem.children
 console.log(numberOfCategories)
-forEach(element of ulEl){
-    return numberOfCategories.elem.children
-}
