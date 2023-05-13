afd8adad995e3e1ef5eeed39eb56a45bdceb347e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-05-13 21:28:34 +0300

    Update task-06.js

--- src 2/js/task-06.js
+++ src 2/js/task-06.js
@@ -2,7 +2,7 @@ let inputEl = document.getElementById('validation-input');
 inputEl.addEventListener('blur',(event)=>{
         let dataLength = inputEl.getAttribute('data-length');
         let eventLength = event.currentTarget.value.length;
-        if (dataLength.includes(eventLength)){
+        if (+dataLength===(eventLength)){
 inputEl.classList.remove('invalid');
 inputEl.classList.add('valid');
 }else{
